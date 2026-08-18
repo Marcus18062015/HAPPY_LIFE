13db4dd6c4b1283c8935c6c3e8e902716543d11b
Marcus18062015 <mmarcus1806@proton.me>
2026-08-18 18:29:05 +0100

    Delete src directory

--- src/components/CarouselRow.tsx
+++
@@ -1,7 +0,0 @@
-export default function CarouselRow({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="-mx-4 flex snap-x snap-mandatory gap-3.5 overflow-x-auto px-4 pb-2 sm:-mx-6 sm:px-6 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
-      {children}
-    </div>
-  );
-}
